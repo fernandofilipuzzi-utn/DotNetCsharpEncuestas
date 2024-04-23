56af8d556adbce80eb8b3834dd5dca9bb7aebae5
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-04-23 09:04:22 -0300

    onsignal event

--- Encuestas/EncuestasMauiApp/App.xaml.cs
+++ Encuestas/EncuestasMauiApp/App.xaml.cs
@@ -3,21 +3,18 @@ using OneSignalSDK.DotNet.Core.Debug;
 
 namespace EncuestasMauiApp
 {
-    public partial class App : Application
+    public partial class App : Application 
     {
         public App()
         {
             InitializeComponent();
 
-
-            OneSignal.Debug.LogLevel = LogLevel.VERBOSE;
-            OneSignal.Initialize("a0d8baa5-b287-4cfd-a465-2c6c872c5535");
-
-            //solicita los permisos al usuario
-            OneSignal.Notifications.RequestPermissionAsync(true);
             
+
+           
 
             MainPage = new AppShell();
         }
+
     }
 }
--- Encuestas/EncuestasMauiApp/MainPage.xaml.cs
+++ Encuestas/EncuestasMauiApp/MainPage.xaml.cs
@@ -11,33 +11,45 @@ namespace EncuestasMauiApp
         public MainPage()
         {
             InitializeComponent();
-            /*
-            // Enable verbose OneSignal logging to debug issues if needed.
+
             OneSignal.Debug.LogLevel = LogLevel.VERBOSE;
 
-            //OneSignal.ConsentRequired = true;
 
-            // OneSignal Initialization
             OneSignal.Initialize("a0d8baa5-b287-4cfd-a465-2c6c872c5535");
-            */
-            // RequestPermissionAsync will show the notification permission prompt.
-            // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 5)
-            /*
+            OneSignal.ConsentRequired = true;
 
-            try
+            OneSignal.User.PushSubscription.Changed += (sender, e) =>
             {
-                OneSignal.Notifications.RequestPermissionAsync(true);
-            }
-            catch (Exception ex)
+                if (e.State.Current.Id != e.State.Previous.Id)
+                {
+                    // OneSignal Subscription id changed.
+                    Shell.Current.DisplayAlert("push", "changed", "ok");
+                }
+            };
+
+            OneSignal.Notifications.Clicked += (sender, e) =>
             {
-                // Manejar el error, por ejemplo, registrándolo o mostrando un mensaje al usuario.
-                Console.WriteLine($"Error al solicitar permisos de notificación: {ex.Message}");
-            }
+                // Access the notification with e.Notification and the click result with e.Result
+                Shell.Current.DisplayAlert("notification", "notification", "ok");
+            };
 
-            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.PostNotifications) != Permission.Granted)
-            { 
+            OneSignal.Notifications.WillDisplay += (sender, e) =>
+            {
+                // Prevent OneSignal from displaying the notification immediately on return.
+                e.PreventDefault();
 
-            }*/
+                // Do some async work
+
+                Shell.Current.DisplayAlert("willdisplay", "willdisplay", "ok");
+
+                // Display the notification after some async work
+                //e.Notification.Display();
+            };
+
+            OneSignal.InAppMessages.WillDisplay += (sender, e) =>
+            {
+                Shell.Current.DisplayAlert("inwilldisplay", "inwilldisplay", "ok");
+            };
         }
 
         private async void btnPrueba_Clicked(System.Object sender, System.EventArgs e)
